5b0a55b6e48c2ae80d2b513959f1012d5d690d7f
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2018-03-22 11:32:41 -0600

    added some more visart, and deployed to heroku

--- seeds/visualart00.js
+++ seeds/visualart00.js
@@ -14,17 +14,17 @@ exports.seed = function(knex, Promise) {
         {
           id: 2,
           title: "The Moon",
-          artist: "Nick Gindraux",
+          artist: "A damn genius",
           url:
-            "http://www.igorstshirts.com/blog/conceptships/2015/nick_gindraux/nick_gindraux_02.jpg",
+            "https://i.pinimg.com/originals/9c/c8/ed/9cc8ed4baf1b3a0a8f8281b668b17745.jpg",
           tags: [0, 1]
         },
         {
           id: 3,
           title: "The Moon",
-          artist: "Nick Gindraux",
+          artist: "Classical Gas",
           url:
-            "http://www.igorstshirts.com/blog/conceptships/2015/nick_gindraux/nick_gindraux_02.jpg",
+            "https://3.bp.blogspot.com/-bIBARK6mN5E/Vv69mUXlfjI/AAAAAAAAWBs/HLv1iSG2p1cqOtcgocnWAbyhOzHHF-4hg/s640/destination%2Bmoon%2B3.jpg",
           tags: [0, 1]
         }
       ]);
