44d6f65491d9c2d932fcb390c2a647e7afbe70e7
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2018-03-22 19:49:51 -0600

    another try at file upload

--- app.js
+++ app.js
@@ -38,51 +38,23 @@ let pdfParser = new PDFParser();
 //     .send(callback);
 // }
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    /*
-          Files will be saved in the 'uploads' directory. Make
-          sure this directory already exists!
-        */
-    cb(null, "./uploads");
-  },
-  filename: (req, file, cb) => {
-    /*
-          uuidv4() will generate a random ID that we'll use for the
-          new filename. We use path.extname() to get
-          the extension from the original file name and add that to the new
-          generated ID. These combined will create the file name used
-          to save the file on the server and will be available as
-          req.file.pathname in the router handler.
-        */
-    const newFilename = `${uuidv4()}${path.extname(file.originalname)}`;
-    cb(null, newFilename);
-  }
-});
-// create the multer instance that will be used to upload/save the file
-const upload = multer({ storage });
-
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.post("/tester", multer({ dest: "./uploads" }).any(), (req, res) => {
+  console.log(req.files);
 
-app.post("/tester", upload.single("selectedFile"), (req, res)=> {
-  res.sendStatus(201);
+  if (!req.files || req.files.length > 1) {
+    return res
+      .status(403)
+      .send("expects only 1 file.")
+      .end();
+  }
+  console.log("it's happening");
+  res.sendStatus(200);
 });
-
-// app.post("/tester", multer({ dest: "./uploads" }).any(), (req, res) => {
-//   console.log(req.files);
-
-//   if (!req.files || req.files.length > 1) {
-//     return res
-//       .status(403)
-//       .send("expects only 1 file.")
-//       .end();
-//   }
-//   res.sendStatus(200);
-// });
 
 // var file1 = req.files.file;
 
