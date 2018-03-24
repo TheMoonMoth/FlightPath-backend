f29824d15c885e3cae9244243f6b18ce84c25200
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2018-03-24 13:27:33 -0600

    all get routes firing

--- app.js
+++ app.js
@@ -1,121 +1,50 @@
+require("dotenv").load();
 const queries = require("./queries");
 
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const multer = require("multer");
+// const multer = require("multer");
+// const multerS3 = require("multer-s3")
 
-const AWS = require("aws-sdk");
-const accessKeyId = process.env.AWS_ACCESS_KEY;
-const secretAccessKey = process.env.AWS_SECRET_KEY;
-
-const fs = require("fs");
-const PDFParser = require("pdf2json");
-const uuidv4 = require("uuid/v4");
-const path = require("path");
-
-let pdfParser = new PDFParser();
-
-// AWS.config.update({
-//   accessKeyId: accessKeyId,
-//   secretAccessKey: secretAccessKey
-// });
-
-// var bucket = new AWS.S3({
-//   params: {
-//     Bucket: "all-submissions"
-//   }
-// });
-
-// function uploadToS3(file, destFileName, callback) {
-//   bucket
-//     .upload({
-//       ACL: "public-read",
-//       Body: fs.createReadStream(file.path),
-//       Key: destFileName.toString(),
-//       ContentType: "application/octet-stream"
-//     })
-//     .send(callback);
-// }
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    /*
-          Files will be saved in the 'uploads' directory. Make
-          sure this directory already exists!
-        */
-    cb(null, "./uploads");
-  },
-  filename: (req, file, cb) => {
-    /*
-          uuidv4() will generate a random ID that we'll use for the
-          new filename. We use path.extname() to get
-          the extension from the original file name and add that to the new
-          generated ID. These combined will create the file name used
-          to save the file on the server and will be available as
-          req.file.pathname in the router handler.
-        */
-    const newFilename = `${uuidv4()}${path.extname(file.originalname)}`;
-    cb(null, newFilename);
-  }
-});
-// create the multer instance that will be used to upload/save the file
-const upload = multer({ storage });
+// const AWS = require("aws-sdk");
+// const accessKeyId = process.env.AWS_ACCESS_KEY;
+// const secretAccessKey = process.env.AWS_SECRET_KEY;
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-app.post("/tester", upload.single("selectedFile"), (req, res)=> {
-  res.sendStatus(201);
-});
-
-// app.post("/tester", multer({ dest: "./uploads" }).any(), (req, res) => {
-//   console.log(req.files);
-
-//   if (!req.files || req.files.length > 1) {
-//     return res
-//       .status(403)
-//       .send("expects only 1 file.")
-//       .end();
-//   }
-//   res.sendStatus(200);
-// });
-
-// var file1 = req.files.file;
-
-// var pid = "10000" + parseInt(Math.random() * 10000000);
-
-// uploadToS3(file1, pid, function (err, data) {
-//   if (err) {
-//     console.error(err);
-//     return res
-//       .status(500)
-//       .send("failed to upload to s3")
-//       .end();
-//   }
-//   res
-//     .status(200)
-//     .send(
-//       "File uploaded to S3: " +
-//       data.Location.replace(/</g, "&lt;") +
-//       '<br/><img src="' +
-//       data.Location.replace(/"/g, "&quot;") +
-//       '"/>'
-//     )
-//     .end();
-// });
-//   }
-// );
-
-app.get("/tester", (req, res) => {
+app.get("/featured", (req, res) => {
   queries
     .listFeatures()
     .then(features => {
       res.json(features);
     })
+    .catch(console.error);
+});
+
+app.get("/poetry", (req, res) => {
+  queries
+    .listPoetry()
+    .then(poetry => {
+      res.json(poetry);
+    })
+    .catch(console.error);
+});
+
+app.get("/fiction", (req, res) => {
+  queries
+    .listFiction()
+    .then(fiction => res.json(fiction))
+    .catch(console.error);
+});
+
+app.get("/visualart", (req, res) => {
+  queries
+    .listArt()
+    .then(art => res.json(art))
     .catch(console.error);
 });
 
--- queries.js
+++ queries.js
@@ -4,28 +4,47 @@ module.exports = {
   listFeatures() {
     return database("featured").select();
   },
-  read(id) {
-    return database("game")
+  readFeature(id) {
+    return database("featured")
       .select()
       .where("id", id)
-      .first();
+      .first()
   },
-  create(game) {
-    return database("game")
-      .insert(game)
-      .returning("*")
-      .then(record => record[0]);
+  listFiction() {
+    return database("fiction").select();
   },
-  update(id, game) {
-    return database("game")
-      .update(game)
-      .where("id", id)
-      .returning("*")
-      .then(record => record[0]);
+  listPoetry() {
+    return database("poetry").select();
   },
-  delete(id) {
-    return database("game")
-      .delete()
-      .where("id", id);
+  listArt() {
+    return database("visualart").select();
   }
 };
+
+
+
+
+// read(id) {
+//     return database("game")
+//       .select()
+//       .where("id", id)
+//       .first();
+//   },
+//   create(game) {
+//     return database("game")
+//       .insert(game)
+//       .returning("*")
+//       .then(record => record[0]);
+//   },
+//   update(id, game) {
+//     return database("game")
+//       .update(game)
+//       .where("id", id)
+//       .returning("*")
+//       .then(record => record[0]);
+//   },
+//   delete(id) {
+//     return database("game")
+//       .delete()
+//       .where("id", id);
+//   }
